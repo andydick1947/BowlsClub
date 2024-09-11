8c8f0c782ec9fb26fcc03ccbc050867cb19867a3
andydick1947 <ardick2000@hotmail.com>
2024-09-11 14:34:55 +0100

    rework for consistency

---
+++ res-bar.js
@@ -0,0 +1,22 @@
+let myRes = ["0","0"]
+
+let myResults = [
+    ["12 APR 24","Ranks 4 12 24.htm"],
+    ["19 APR 24","Ranks 4 19 24.htm"],
+    ["26 APR 24","Ranks 4 26 24.htm"],
+    ["17 MAY 24","Ranks 5 17 24.htm"],
+    ["24 MAY 24","Ranks 5 24 24.htm"],
+    ["14 JUN 24","Ranks 6 14 24.htm"],
+    ["21 JUN 24","Ranks 6 21 24.htm"],
+    ["5 JUL 24","Ranks 7 5 24.htm"],
+    ["12 JUL 24","Ranks 7 12 24.htm"],
+    ["19 JUL 24","Ranks 7 19 24.htm"],
+    ["26 JUL 24","Ranks 7 26 24.htm"],
+    ["2 AUG 24","Ranks 8 2 24.htm"],
+    ["9 AUG 24","Ranks 8 9 24.htm"],
+    ["16 AUG 24","Ranks 8 16 24.htm"],
+    ["30 AUG 24","Ranks 8 30 24.htm"],
+    ["6 SEP 24","Ranks 9 6 24.htm"]
+    
+    ]
+

22151eaf8fed009df3b852c896987faadd336df8
andydick1947 <ardick2000@hotmail.com>
2025-08-27 14:48:32 +0100

    add test

--- index.js
+++ index.js
@@ -285,3 +285,18 @@ for (let x=1; x<=8; x++){
 
    })
    }
+
+  window.addEventListener("load",function(){
+      setTimeout(
+         function open(event){
+      document.querySelector(".popup").style.display="block";
+   }),
+   1000
+   });
+
+
+   document.querySelector("#close").addEventListener("click",function(){
+      document.querySelector(".popup").style.display="none";
+   });
+
+ 
